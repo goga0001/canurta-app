1a2a6f7ab71a924024c23b316a6d5d75de70d364
goga0001 <hsjhsj127@gmail.com>
2022-08-19 00:08:10 -0400

    update model.py

--- apps/model.py
+++ apps/model.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 def app():
     st.title('Profile')
+    st.write("profile (where the user can input to capture pain, mood")
+    st.write("share data to doctor using the share symbol (get the doctors info/email), maybe use fullscript as link to connect to portal)")
 
 
 

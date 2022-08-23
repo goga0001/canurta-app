aa58aeb23a794d3f3c68ecd09de63b4492139f33
goga0001 <hsjhsj127@gmail.com>
2022-08-22 23:30:14 -0400

    update app.py

--- apps/data.py
+++ apps/data.py
@@ -12,18 +12,16 @@ def app():
     st.title('Data')
     lang = pd.read_excel('Book1.xlsx')
 
-    with st.expander(" Choose your filters"):
-      list= lang.columns[1:8].tolist()
-      choice= st.multiselect("Pick your biomarker", list, list[:8])
-      start = st.date_input("Start", value=pd.to_datetime("2022-01-01"))
-      end = st.date_input("End", value=pd.to_datetime("2022-03-31"))
+  
+    list= lang.columns[1:8].tolist()
+    choice= list
     groupby_column2 = st.multiselect(
         'What would you like to analyse?',
         ('trail', 'crp', 'il6', "tgfb",	"tgfa",	"il8","ip10")
     )
     data=lang[choice]
     st.text("A close look into the data")
-    st.line_chart(data)
+     
 
     fig2 = px.line(
         data,
@@ -40,14 +38,19 @@ def app():
     data1=lang[choice1]
     groupby_column = st.selectbox(
         'What would you like to analyse?',
-        ("rhr","calories","skin temp", "sleep")
+        ("rhr","calories","skin_temp", "sleep")
     )
 
     fig1 = px.bar(
         data1,
         x=lang['date'],
-        y=groupby_column)
-    st.plotly_chart(fig1)
+        y=groupby_column,
+        color_continuous_scale=['red', 'yellow', 'green'],
+        template='plotly_white',
+        title=f'<b>Physiological Analysis by {groupby_column}</b>'
+        )
+    fig1.update_xaxes(title_text='Date')
+    st.plotly_chart(fig1,use_container_width=True)
 
 
 
